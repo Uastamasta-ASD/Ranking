bc7514d58ec5141b701311b467716ddcfda4c6a2
fren_gor <goro@frengor.com>
2024-10-24 13:09:09 +0200

    Add documentation

--- src/traits.rs
+++ src/traits.rs
@@ -1,10 +1,15 @@
+/// Trait representing a bacchiatore.
 pub trait Bacchiatore {
+    /// Returns the current elo of the bacchiatore.
     fn elo(&self) -> i32;
 
+    /// Returns the number of *already-ranked* duels played by this bacchiatore.
     fn total_duels(&self) -> u32;
 
+    /// Returns the number of days in which this bacchiatore played an *already-ranked* duels.
     fn total_days(&self) -> u32;
 
+    /// Called at the end of the elo calculation with the elo increase/decrease for this bacchiatore.
     fn elo_delta_callback(&mut self, elo_delta: i32);
 }
 
@@ -41,13 +46,18 @@ impl<T: Bacchiatore> Bacchiatore for &mut T {
 
 impl_bacchiatore!(Box);
 
+/// Trait representing a duel.
 pub trait Duel {
+    /// Returns the points done by equal.
     fn equal_points(&self) -> i32;
 
+    /// Returns the points done by opposite.
     fn opposite_points(&self) -> i32;
 
+    /// Called at the end of the elo calculation with the elo increase/decrease for equal.
     fn equal_elo_delta_callback(&mut self, elo_delta: i32);
 
+    /// Called at the end of the elo calculation with the elo increase/decrease for opposite.
     fn opposite_elo_delta_callback(&mut self, elo_delta: i32);
 }
 
